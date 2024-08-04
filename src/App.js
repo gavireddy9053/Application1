c51c086d0aec347111bb67b09d36d7e6d3be416c
Gavireddy Linitha <gavireddy9053@gmail.com>
2024-08-04 10:23:57 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Offers from './components/Offers'
 import Help from './components/Help'
 import Cart from './components/Cart'
 import MenuItems from './components/MenuItems'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 import { createContext } from 'react'
 
 function App(){
